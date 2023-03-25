a4f69baf3c5daed20b9c997de520d8a1121129c5
ElAbdelali <eljaouhariabdelali@outlook.com>
2023-03-25 08:41:12 -0500

    Completed exercises 1 and 2 from 3. Think Like a Computer Scientist: Strings

---
+++ Python-Coursework/Prework/Strings/9_22_1.py
@@ -0,0 +1,22 @@
+
+
+# What is the result of each of the following:
+#     ‘Python’[1]
+#     “Strings are sequences of characters.”[5]
+#     len(“wonderful”)
+#     ‘Mystery’[:4]
+#     ‘p’ in ‘Pineapple’
+#     ‘apple’ in ‘Pineapple’
+#     ‘pear’ not in ‘Pineapple’
+#     ‘apple’ > ‘pineapple’
+#     ‘pineapple’ < ‘Peach’
+
+# ‘Python’[1] #y
+# “Strings are sequences of characters.”[5] #g
+# len(“wonderful”) #9
+# ‘Mystery’[:4] #Myst
+# ‘p’ in ‘Pineapple’ #true
+# ‘apple’ in ‘Pineapple’ #true
+# ‘pear’ not in ‘Pineapple’ #true
+# ‘apple’ > ‘pineapple’ #false
+# ‘pineapple’ < ‘Peach’ #false
